121d2b886380985e111731b4ef5c573b5141ad58
blackghost86 <andrei.voicu@ubisoft.com>
2026-08-24 13:35:22 +0300

    Add files via upload

--- TQLauncher/Models/MissionPack.cs
+++ TQLauncher/Models/MissionPack.cs
@@ -1,10 +1,51 @@
-﻿namespace TinyQuakeLauncher.Models;
+﻿using System.IO;
+
+namespace TinyQuakeLauncher.Models;
 
 public class MissionPack
 {
     public string Name { get; set; } = "";
 
-    public string GameDirectory { get; set; } = "";
+    public List<string> PossibleDirectories { get; set; } = new();
+
+    public string? DetectedDirectory { get; set; }
+
+    public string GameDirectory
+    {
+        get
+        {
+            return DetectedDirectory ?? "";
+        }
+    }
+
+    public bool TryDetectDirectory(string quakeFolder)
+    {
+        foreach (string directory in PossibleDirectories)
+        {
+            if (string.IsNullOrWhiteSpace(directory))
+            {
+                if (Directory.Exists(quakeFolder))
+                {
+                    DetectedDirectory = "";
+                    return true;
+                }
+
+                continue;
+            }
+
+            string fullPath =
+                Path.Combine(quakeFolder, directory);
+
+            if (Directory.Exists(fullPath))
+            {
+                DetectedDirectory = directory;
+                return true;
+            }
+        }
+
+        DetectedDirectory = null;
+        return false;
+    }
 
     public override string ToString()
     {
