1ab2faa1181eb971084a81708eca14c7caa9fbb3
blackghost86 <andrei.voicu@ubisoft.com>
2026-08-28 13:11:55 +0300

    Add files via upload

--- TQLauncher/Models/MapInfo.cs
+++ TQLauncher/Models/MapInfo.cs
@@ -6,13 +6,16 @@ public class MapInfo
 
     public string Title { get; set; } = "";
 
+    public System.Windows.Media.Brush Foreground { get; set; } =
+        System.Windows.Media.Brushes.Black;
+
+    public string DisplayName =>
+        string.IsNullOrWhiteSpace(Title)
+            ? FileName
+            : $"{FileName} | {Title}";
+
     public override string ToString()
     {
-        if (string.IsNullOrWhiteSpace(Title))
-        {
-            return FileName;
-        }
-
-        return $"{FileName} | {Title}";
+        return DisplayName;
     }
 }
